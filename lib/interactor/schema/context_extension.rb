19180afc557eb733b277afd5d844f1c518938428
Bernardo Farah <ber@bernardo.me>
2017-01-30 17:16:33 -0800

    Fixing Context Extension
    Super does not exist here
    
    Also, `schema.rb` will keep track of all the dependencies, so we can
    remove them from here

--- lib/interactor/schema/context_extension.rb
+++ lib/interactor/schema/context_extension.rb
@@ -1,13 +1,10 @@
-require "interactor/context"
-require "interactor/schema/context"
-
 module Interactor
   class Context
     # Overriding build method to continue with a Schema Context
     # when one is found
     def self.build(context = {})
       return context if context.is_a?(Interactor::Schema::Context)
-      super
+      self === context ? context : new(context)
     end
   end
 end
